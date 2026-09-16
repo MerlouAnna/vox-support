f2d97033f24186d280a24c9e9a1c0b3167da89fc
Anna Merlou <anna.merlou@satorianalytics.com>
2026-09-16 14:13:53 +0300

    CI Checks - run quality gates on pull requests

--- src/vox/main.py
+++ src/vox/main.py
@@ -5,8 +5,6 @@ from fastapi import FastAPI
 from vox.api.v1.router import api_router
 from vox.core.config import Settings, get_settings
 
-settings = get_settings()
-
 
 # this is a "factory" function that creates and configures the FastAPI app instance
 def create_app(settings: Settings | None = None) -> FastAPI:
@@ -17,6 +15,7 @@ def create_app(settings: Settings | None = None) -> FastAPI:
         version=version("vox"),
         docs_url=None if settings.environment == "production" else "/docs",
     )
-    # v1 is wired here w/o knowing the endpoints in it - this will pass all requests to the appropriate routers
+    # v1 is wired here w/o knowing the endpoints in it
+    # this will pass all requests to the appropriate routers
     app.include_router(api_router, prefix="/api/v1")
     return app
--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,6 @@ from vox.main import create_app
 def client() -> Iterator[TestClient]:
     settings = Settings(_env_file=None, environment="local")
     app = create_app(settings=settings)
-    with (
-        TestClient(app) as test_client
-    ):  # this is a context mnanager - entering it runs __enter__ and exiting runs __exit__
+    with TestClient(app) as test_client:  # this is a context mnanager
+        # entering it runs __enter__ and exiting runs __exit__
         yield test_client
